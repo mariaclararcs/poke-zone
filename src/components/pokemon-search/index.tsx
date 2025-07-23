dbdfb8a67cb88363d9eba59a9c8b9b011c257b05
mariaclararcs <mariaclarar981@gmail.com>
2025-07-23 18:48:34 -0300

    feat(pokemon search): add basic filters

--- src/components/pokemon-search/index.tsx
+++ src/components/pokemon-search/index.tsx
@@ -4,6 +4,7 @@ import { Search } from "lucide-react"
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
 import { FormEvent, useState } from "react"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
 
 interface PokemonSearchProps {
   onSearch: (searchTerm: string) => void
@@ -26,31 +27,47 @@ export default function PokemonSearch({ onSearch }: PokemonSearchProps) {
     }
 
     return (
-        <div className="w-full flex flex-col gap-2 items-center justify-center px-4 sm:px-8 md:px-12 lg:px-20 py-4 xl:py-6">
-            <form onSubmit={handleSubmit}
-                className="relative flex w-full max-w-md items-center gap-2">
-                <Input 
-                    type="text" 
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    className="pr-12"
-                />
-                <Button 
-                    type="submit" 
-                    className="absolute right-3 top-1/2 -translate-y-1/2 bg-background border-1 border-input text-foreground hover:bg-muted-foreground"
-                    aria-label="Search Pokémon"
-                >
-                    <Search className="h-4 w-4" />
-                </Button>
-            </form>
+        <div className="w-full flex flex-row gap-6 items-center justify-center px-4 sm:px-8 md:px-12 lg:px-20 py-4 xl:py-6">
+            <div className="flex flex-col gap-2">
+                <form onSubmit={handleSubmit}
+                    className="relative flex w-full max-w-md items-center gap-2">
+                    <Input 
+                        type="text" 
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="pr-12"
+                    />
+                    <Button 
+                        type="submit" 
+                        className="absolute right-3 top-1/2 -translate-y-1/2 bg-background border-1 border-input text-foreground hover:bg-muted-foreground"
+                        aria-label="Search Pokémon"
+                    >
+                        <Search className="h-4 w-4" />
+                    </Button>
+                </form>
 
-            {error && (
-                <p className="text-sm text-red-500">{error}</p>
-            )}
-            
-            <p className="text-sm text-muted-foreground">
-                Search for a Pokémon by name or Pokédex number
-            </p>
+                {error && (
+                    <p className="text-sm text-red-500">{error}</p>
+                )}
+                
+                <p className="text-sm text-muted-foreground">
+                    Search for a Pokémon by name or Pokédex number
+                </p>
+            </div>
+
+            <div>
+                <Select>
+                    <SelectTrigger className="w-60">
+                        <SelectValue placeholder="Filters" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="lowest">Lowest Number (First)</SelectItem>
+                        <SelectItem value="highest">Highest Number (First)</SelectItem>
+                        <SelectItem value="az">A-Z</SelectItem>
+                        <SelectItem value="za">Z-A</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
         </div>
     )
 }
